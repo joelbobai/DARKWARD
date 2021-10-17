95db0eedf37229c243f34ec1fdc613b04f5636d7
joelbobai <90658305+joelbobai@users.noreply.github.com>
2021-10-17 14:10:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
- var y = document.getElementById("setting-menu"); 
+
+var x = document.querySelector(".setting-menu");
 
 
-  function settingsMenuToggle() {
-     y.style.height = "430px"
+function settingsMenuToggle() {
+x.classList.toggle("show");
+}
+
+
+var y = document.querySelector(".light-btn");
+
+function onIt() {
+   y.classList.toggle("light-btn-on");
+   document.body.classList.toggle("light-theme");
+
+   if (localStorage.getItem("theme") == "dark") {
+        localStorage.setItem("theme", "light");
+   }
+   else{
+      localStorage.setItem("theme", "light");
+   }
+}
+
+
+
+
+if (localStorage.getItem("theme") == "dark") {
+    y.classList.remove("light-btn-on");
+    document.body.classList.remove("light-theme");
+}
+
+else if(localStorage.getItem("theme") == "light"){
+   y.classList.add("light-btn-on");
+    document.body.classList.add("light-theme");
 
 }
 
-var lightbtn = document.getElementById("light-btn");
 
-     lightbtn.onclick = function() {
-     	lightbtn.classList.toggle("light-btn-on")
-     }
+
+else{
+   localStorage.setItem("theme", "light");
+}
